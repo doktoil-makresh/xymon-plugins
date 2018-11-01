72943d5b903da4c81c26af9a98f7746ad3cc6d6f
Damien Martins <dmartins@anevia.com>
2018-11-01 15:34:33 +0100

    Per folder organization

---
+++ dedibackup/dedibackup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#
+#Xymon variables
+TEST = backup
+#Quota dedibox en Mo
+DEDI_QUOTA=200000
+#Retention souhaitee des donnees :
+RETENTION=15
+#Retention minimale :
+RETENTION_MINIMAL=5
+#Intervalle entre les tests :
+INTERVAL=1d
+
+#Recuperation des donnees
+
+#Calcul de la taille dispo
+
+#Calcul de la taille de la derniere sauvegarde
+
+#Verification de la possibilite de stocker la prochaine sauvegarde, en extrapolant de la taille de celle du jour
+
+#Verification de la taille des sauvegardes
+
+#Mise en forme des donnees de base
+
+#Verification de la presence des fichiers
+
+#Envoi des donnes à Xymon
+
