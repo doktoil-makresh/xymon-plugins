080f39a77b077a59f07527991ddc1b077f995e17
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-11-15 17:32:09 +0100

    Lighter xymon sensors script

--- xymon-home_sensors/xymon-home_sensors.py
+++ xymon-home_sensors/xymon-home_sensors.py
@@ -7,7 +7,53 @@ import os
 #Import config file
 import configparser
 config = configparser.ConfigParser()
-config.read(os.environ['XYMONCLIENTHOME']+'/etc/xymon-home_sensors.ini')
+debug = 'XYMONCLIENTHOME' not in os.environ
+if debug:
+	config.read('xymon-home_sensors.ini')
+else:
+	config.read(os.environ['XYMONCLIENTHOME']+'/etc/xymon-home_sensors.ini')
+class Variable():
+	def __init__(self, name):
+		self.name = name
+		self.value = float('NaN')
+		self.thresholds = {
+			"warning": (-sys.maxsize, sys.maxsize),
+			"alarm": (-sys.maxsize, sys.maxsize)
+		}
+	
+	@property
+	def color(self):
+		if self.value < self.thresholds['alarm'][0] or self.value > self.thresholds['alarm'][1]:
+			return "red"
+		elif self.value < self.thresholds['warning'][0] or self.value > self.thresholds['warning'][1]:
+			return "yellow"
+		else:
+			return "green"
+
+class Sensor:
+	def __init__(self, name):
+		self.name = name
+		self.variables = {
+			"temperature": Variable("temp"),
+			"humidity": Variable("humidity")
+		}
+
+	def read_thresholds(self):
+		for k in self.variables:
+			v = self.variables[k]
+			# Read thresholds as tuples
+			self.variables[k].thresholds['warning'] = (float(config[k][self.name + "_" + v.name + "_warn_min"]), float(config[k][self.name + "_" + v.name + "_warn_max"]))
+			self.variables[k].thresholds['alarm'] = (float(config[k][self.name + "_" + v.name + "_alarm_min"]), float(config[k][self.name + "_" + v.name + "_alarm_max"]))
+
+	def __str__(self):
+		return "\n".join(["&%s%s_%s: %.1f" % (v.color, self.name.capitalize(), v.name, v.value) for v in self.variables.values()])
+
+sensors = {
+	"ground": Sensor('ground'),
+	"floor": Sensor('floor'),
+	"veranda": Sensor('veranda'),
+	"garage": Sensor('garage')
+}
 
 #Define variables from config file
 #Adapt for debug
@@ -16,207 +62,38 @@ _status = "green"
 _test = "home_sensors"
 now=datetime.datetime.now()
 _date=now.strftime("%a %d %b %Y %H:%M:%S %Z")
-red = 0
-yellow = 0
 
-#set threshold
-ground_temp_warn_min = float(config['temperature']['ground_temp_warn_min'])
-ground_temp_warn_max = float(config['temperature']['ground_temp_warn_max'])
-ground_temp_alarm_min = float(config['temperature']['ground_temp_alarm_min'])
-ground_temp_alarm_max = float(config['temperature']['ground_temp_alarm_max'])
-ground_humidity_warn_min = float(config['humidity']['ground_humidity_warn_min'])
-ground_humidity_warn_max = float(config['humidity']['ground_humidity_warn_max'])
-ground_humidity_alarm_min = float(config['humidity']['ground_humidity_alarm_min'])
-ground_humidity_alarm_max = float(config['humidity']['ground_humidity_alarm_max'])
-
-floor_temp_warn_min = float(config['temperature']['floor_temp_warn_min'])
-floor_temp_warn_max = float(config['temperature']['floor_temp_warn_max'])
-floor_temp_alarm_min = float(config['temperature']['floor_temp_alarm_min'])
-floor_temp_alarm_max = float(config['temperature']['floor_temp_alarm_max'])
-floor_humidity_warn_min = float(config['humidity']['floor_humidity_warn_min'])
-floor_humidity_warn_max = float(config['humidity']['floor_humidity_warn_max'])
-floor_humidity_alarm_min = float(config['humidity']['floor_humidity_alarm_min'])
-floor_humidity_alarm_max = float(config['humidity']['floor_humidity_alarm_max'])
-
-veranda_temp_warn_min = float(config['temperature']['veranda_temp_warn_min'])
-veranda_temp_warn_max = float(config['temperature']['veranda_temp_warn_max'])
-veranda_temp_alarm_min = float(config['temperature']['veranda_temp_alarm_min'])
-veranda_temp_alarm_max = float(config['temperature']['veranda_temp_alarm_max'])
-veranda_humidity_warn_min = float(config['humidity']['veranda_humidity_warn_min'])
-veranda_humidity_warn_max = float(config['humidity']['veranda_humidity_warn_max'])
-veranda_humidity_alarm_min = float(config['humidity']['veranda_humidity_alarm_min'])
-veranda_humidity_alarm_max = float(config['humidity']['veranda_humidity_alarm_max'])
-
-garage_temp_warn_min = float(config['temperature']['garage_temp_warn_min'])
-garage_temp_warn_max = float(config['temperature']['garage_temp_warn_max'])
-garage_temp_alarm_min = float(config['temperature']['garage_temp_alarm_min'])
-garage_temp_alarm_max = float(config['temperature']['garage_temp_alarm_max'])
-garage_humidity_warn_min = float(config['humidity']['garage_humidity_warn_min'])
-garage_humidity_warn_max = float(config['humidity']['garage_humidity_warn_max'])
-garage_humidity_alarm_min = float(config['humidity']['garage_humidity_alarm_min'])
-garage_humidity_alarm_max = float(config['humidity']['garage_humidity_alarm_max'])
+for sensor in sensors.values():
+	sensor.read_thresholds()
 
 #get_values
 with open(_Source_File, 'r') as f:
 	for row in f:
 		if row == "No data received from WeatherStation":
 			sys.exit(1)
-		ground_temp = float(row.split(',')[0])
-		ground_humidity = float(row.split(',')[1])
-		outdoor_temp = float(row.split(',')[2])
-		outdoor_humidity = float(row.split(',')[3])
-		floor_temp = float(row.split(',')[4])
-		floor_humidity = float(row.split(',')[5])
-		veranda_temp = float(row.split(',')[6])
-		veranda_humidity = float(row.split(',')[7])
-    garage_temp = float(row.split(',')[7])
-		garage_humidity = float(row.split(',')[8])
+		values = [float(x) for x in row.split(",")]
+		sensors['ground'].variables['temperature'].value = values[0]
+		sensors['ground'].variables['humidity'].value = values[1]
+		sensors['floor'].variables['temperature'].value = values[2]
+		sensors['floor'].variables['humidity'].value = values[3]
+		sensors['veranda'].variables['temperature'].value = values[4]
+		sensors['veranda'].variables['humidity'].value = values[5]
+		sensors['garage'].variables['temperature'].value = values[6]
+		sensors['garage'].variables['humidity'].value = values[7]
 
-f.close()
-#check_values
-##ground_floor
-###temperature
-if ground_temp > floor_temp_alarm_max:
-	red = 1
-	ground_temp_color = "red"
-elif ground_temp > ground_temp_warn_max:
-	yellow = 1
-	ground_temp_color = "yellow"
-elif ground_temp < ground_temp_alarm_min:
-	red = 1
-	ground_temp_color = "red"
-elif ground_temp < ground_temp_warn_min:
-	yellow = 1
-	ground_temp_color = "yellow"
+# Check for colors
+for sensor in sensors.values():
+	for variable in sensor.variables.values():
+		if variable.color == "red":
+			_status = "red"
+		elif variable.color == "yellow" and _status != "red":
+			_status = "yellow"
+
+if debug:
+	_msg_line="\n".join([str(sensor) for sensor in sensors.values()])
+	print(_msg_line)
 else:
-	ground_temp_color = "green"	
-
-###humidity
-if ground_humidity > ground_humidity_alarm_max:
-	red = 1
-	ground_humidity_color = "red"
-elif ground_humidity > ground_humidity_warn_max:
-	yellow = 1
-	ground_humidity_color = "yellow"
-elif ground_humidity < ground_humidity_alarm_min:
-	red = 1
-	ground_humidity_color = "red"
-elif ground_humidity < ground_humidity_warn_min:
-	yellow = 1
-	ground_humidity_color = "yellow"
-else:
-	ground_humidity_color = "green"	
-
-##first floor
-###temperature
-if floor_temp > floor_temp_alarm_max:
-	red = 1
-	floor_temp_color = "red"
-elif floor_temp > ground_temp_warn_max:
-	yellow = 1
-	floor_temp_color = "yellow"
-elif floor_temp < ground_temp_alarm_min:
-	red = 1
-	floor_temp_color = "red"
-elif floor_temp < ground_temp_warn_min:
-	yellow = 1
-	floor_temp_color = "yellow"
-else:
-	floor_temp_color = "green"	
-
-###humidity
-if floor_humidity > floor_humidity_alarm_max:
-	red = 1
-	floor_humidity_color = "red"
-elif floor_humidity > ground_humidity_warn_max:
-	yellow = 1
-	floor_humidity_color = "yellow"
-elif floor_humidity < ground_humidity_alarm_min:
-	red = 1
-	floor_humidity_color = "red"
-elif floor_humidity < ground_humidity_warn_min:
-	yellow = 1
-	floor_humidity_color = "yellow"
-else:
-	floor_humidity_color = "green"	
-
-##Veranda
-###temperature
-if veranda_temp > veranda_temp_alarm_max:
-	red = 1
-	veranda_temp_color = "red"
-elif veranda_temp > veranda_temp_warn_max:
-	yellow = 1
-	veranda_temp_color = "yellow"
-elif veranda_temp < veranda_temp_alarm_min:
-	red = 1
-	veranda_temp_color = "red"
-elif veranda_temp < veranda_temp_warn_min:
-	yellow = 1
-	veranda_temp_color = "yellow"
-else:
-	veranda_temp_color = "green"	
-
-###humidity
-if veranda_humidity > veranda_humidity_alarm_max:
-	red = 1
-	veranda_humidity_color = "red"
-elif veranda_humidity > veranda_humidity_warn_max:
-	yellow = 1
-	veranda_humidity_color = "yellow"
-elif veranda_humidity < veranda_humidity_alarm_min:
-	red = 1
-	veranda_humidity_color = "red"
-elif veranda_humidity < veranda_humidity_warn_min:
-	yellow = 1
-	veranda_humidity_color = "yellow"
-else:
-	veranda_humidity_color = "green"	
-
-##Garage
-###temperature
-if garage_temp > garage_temp_alarm_max:
-  red = 1
-  garage_temp_color = "red"
-elif garage_temp > garage_temp_warn_max:
-  yellow = 1
-  garage_temp_color = "yellow"
-elif garage_temp < garage_temp_alarm_min:
-  red = 1
-  garage_temp_color = "red"
-elif garage_temp < garage_temp_warn_min:
-  yellow = 1
-  garage_temp_color = "yellow"
-else:
-  garage_temp_color = "green"
-
-###humidity
-if garage_humidity > garage_humidity_alarm_max:
-  red = 1
-  garage_humidity_color = "red"
-elif garage_humidity > garage_humidity_warn_max:
-  yellow = 1
-  garage_humidity_color = "yellow"
-elif garage_humidity < garage_humidity_alarm_min:
-  red = 1
-  garage_humidity_color = "red"
-elif garage_humidity < garage_humidity_warn_min:
-  yellow = 1
-  garage_humidity_color = "yellow"
-else:
-  garage_humidity_color = "green"
-
-
-#Generate global status:
-if red == 1:
-	_status = "red"
-elif yellow == 1:
-	_status = "yellow"
-else:
-	_status = "green"
-
-_msg_line="&%sIndoor_temp: %s\n&%sIndoor_humidity: %s\n&%sFloor_temp: %s\n&%sFloor_humidity: %s\n&%sVeranda_temp: %s\n&%sVeranda_humidity: %s\nOutdoor_temp: %s\nOutdoor_humidity: %s\n" % (ground_temp_color, ground_temp, ground_humidity_color, ground_humidity, floor_temp_color, floor_temp, floor_humidity_color, floor_humidity, veranda_temp_color, veranda_temp, veranda_humidity_color, veranda_humidity, outdoor_temp, outdoor_humidity)
-
-#Lancement commande 
-_cmd_line="%s %s \"status %s.%s %s %s\n\n%s\"" %(os.environ['XYMON'], os.environ['XYMSRV'], os.environ['MACHINE'], _test, _status, _date, _msg_line)
-os.system(_cmd_line)
+	_msg_line="&%sIndoor_temp: %s\n&%sIndoor_humidity: %s\n&%sFloor_temp: %s\n&%sFloor_humidity: %s\n&%sVeranda_temp: %s\n&%sVeranda_humidity: %s\nOutdoor_temp: %s\nOutdoor_humidity: %s\n" % (ground_temp_color, ground_temp, ground_humidity_color, ground_humidity, floor_temp_color, floor_temp, floor_humidity_color, floor_humidity, veranda_temp_color, veranda_temp, veranda_humidity_color, veranda_humidity, outdoor_temp, outdoor_humidity)
+	_cmd_line="%s %s \"status %s.%s %s %s\n\n%s\"" %(os.environ['XYMON'], os.environ['XYMSRV'], os.environ['MACHINE'], _test, _status, _date, _msg_line)
+	#Lancement commande 
+	os.system(_cmd_line)
